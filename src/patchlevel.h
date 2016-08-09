8ba2cad51a968177a4ea3f34e107beaa84323a94
Marisa Giancarla <marisag@mekcity.com>
2016-08-09 03:29:56 +0000

    Bumped version number

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -5,8 +5,8 @@
 
 #include "copyright.h"
 
-#define	MUX_VERSION		"1.6 (btech 1.4.3+)"	/* Base version number */
+#define	MUX_VERSION		"1.6 (btech 1.4.5+)"	/* Base version number */
 #define PATCHLEVEL		0	/* Patch sequence number */
-#define	MUX_RELEASE_DATE	"26/02/02"	/* Source release date */
+#define	MUX_RELEASE_DATE	"08/08/16"	/* Source release date */
 
 #define BETA			0	/* Define if a BETA release */
